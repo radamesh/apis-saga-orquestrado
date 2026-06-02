05f3423f1f25b233f0abe911ff3365172065bbf2
Radamés Silva <radamesh@gmail.com>
2026-06-02 11:03:04 -0300

    implementando o inventory no projeto

--- inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
+++ inventory-service/src/main/java/br/com/microservices/orchestrated/inventoryservice/core/consumer/InventoryConsumer.java
@@ -1,5 +1,6 @@
 package br.com.microservices.orchestrated.inventoryservice.core.consumer;
 
+import br.com.microservices.orchestrated.inventoryservice.core.service.InventoryService;
 import br.com.microservices.orchestrated.inventoryservice.core.utils.JsonUtil;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 public class InventoryConsumer {
 
     private final JsonUtil jsonUtil;
+    private final InventoryService inventoryService;
 
     @KafkaListener(
         groupId = "${spring.kafka.consumer.group-id}",
@@ -20,16 +22,18 @@ public class InventoryConsumer {
     public void consumerSuccessEvent(String payload) {
         log.info("Received success event {} from inventory-success", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        // log.info(event.toString());
+        inventoryService.updateInventory(event);
     }
 
     @KafkaListener(
-            groupId = "${spring.kafka.consumer.group-id}",
-            topics = "${spring.kafka.topic.inventory-fail}"
+        groupId = "${spring.kafka.consumer.group-id}",
+        topics = "${spring.kafka.topic.inventory-fail}"
     )
     public void consumerFailEvent(String payload) {
         log.info("Received rollback event {} from inventory-fail", payload);
         var event = jsonUtil.toEvent(payload);
-        log.info(event.toString());
+        // log.info(event.toString());
+        inventoryService.rollbackInventory(event);
     }
 }
---
+++ inventory-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into public.inventory (id, product_code, available) values (1, 'COMIC_BOOKS', 4);
+insert into public.inventory (id, product_code, available) values (2, 'BOOKS', 2);
+insert into public.inventory (id, product_code, available) values (3, 'MOVIES', 5);
+insert into public.inventory (id, product_code, available) values (4, 'MUSIC ', 9);
