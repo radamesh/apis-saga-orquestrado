73f7eecacc506595375a2f0cb78a594595379ee2
Radamés Silva <radamesh@gmail.com>
2026-06-06 17:25:43 -0300

    ajustando coleçoes do mongo DB

--- order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
+++ order-service/src/main/java/br/com/microservices/orchestrated/orderservice/core/document/Order.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collation = "order")
+@Document(collection = "order")
 public class Order {
     @Id
     private String id;
